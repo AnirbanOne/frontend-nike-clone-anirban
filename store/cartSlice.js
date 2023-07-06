742ebbc71a76b6abb3d418f9242c9a0a04e39a85
AnirbanOne <anirban.chk1@gmail.com>
2023-07-06 21:07:40 +0530

    Frontend backup

---
+++ store/cartSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cartItems: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+        const item = state.cartItems.find((p) => p.id === action.payload.id);
+        if (item) {
+          const updatedItem = {
+            ...item,
+            quantity: item.quantity + 1,
+            attributes: {
+              ...item.attributes,
+              price: item.oneQuantityPrice * (item.quantity + 1)
+            }
+          };
+          state.cartItems = state.cartItems.map((p) =>
+            p.id === action.payload.id ? updatedItem : p
+          );
+        } else {
+          state.cartItems.push({ ...action.payload, quantity: 1 });
+        }
+      },      
+    updateCart: (state, action) => {
+      state.cartItems = state.cartItems.map((p) => {
+        if (p.id === action.payload.id) {
+          if (action.payload.key === "quantity") {
+            p.attributes.price = p.oneQuantityPrice * action.payload.val;
+          }
+          return { ...p, [action.payload.key]: action.payload.val };
+        }
+        return p;
+      });
+    },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (p) => p.id !== action.payload.id
+      );
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToCart, updateCart, removeFromCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
